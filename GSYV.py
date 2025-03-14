b87408a95523a8a95caa7ab3f9840e1bc8158bf8
Mustafa AKBAL <mstf.akbal@gmail.com>
2025-03-15 00:42:28 +0300

    Update GSYV.py

--- GSYV.py
+++ GSYV.py
@@ -2302,11 +2302,14 @@ class InventoryApp(QMainWindow):
                     if "DejaVuSans" not in pdfmetrics.getRegisteredFontNames():
                         font_path = resource_path(os.path.join("files", "DejaVuSans.ttf"))
                         if os.path.exists(font_path):
-                            pdfmetrics.registerFont(TTFont("DejaVuSans", font_path))
-                            logging.info(f"DejaVuSans.ttf yüklendi: {font_path}")
+                            #pdfmetrics.registerFont(TTFont("DejaVuSans", font_path))
+                            #logging.info(f"DejaVuSans.ttf yüklendi: {font_path}")
+                            pdfmetrics.registerFont(TTFont("DejaVuSans", "C:\DejaVuSans.ttf"))
+                            logging.info(f"DejaVuSans.ttf yüklendi: C:\DejaVuSans.ttf")
                         else:
                             logging.warning("DejaVuSans.ttf bulunamadı, Helvetica kullanılıyor.")
-                            pdfmetrics.registerFont(TTFont("Helvetica", "Helvetica"))  # Yedek font
+                            #pdfmetrics.registerFont(TTFont("Helvetica", "Helvetica"))  # Yedek font
+                            pdfmetrics.registerFont(TTFont("Helvetica", "C:\Helvetica.ttf"))  # Yedek font
                             QMessageBox.warning(self, "Uyarı", "Türkçe karakter desteği için DejaVuSans.ttf bulunamadı.")
 
                     # Başlık
